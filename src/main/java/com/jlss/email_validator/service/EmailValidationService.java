391d7c8f0c8a770ef10e99a53e0bcc196577cd91
Jeet-solanki-1 <solankijeet285@gmail.com>
2026-03-30 06:12:15 +0530

    (DNS-Lookup-done): implemented dns lookup from org.xbill.DNS.* and used fallback technique on Type.A records also. created a MXLookupResult subclass static one

--- src/main/java/com/jlss/email_validator/service/EmailValidationService.java
+++ src/main/java/com/jlss/email_validator/service/EmailValidationService.java
@@ -2,7 +2,15 @@ package com.jlss.email_validator.service;
 
 import org.springframework.stereotype.Service;
 import com.jlss.email_validator.model.EmailValidationResponse;
+
 import java.util.regex.Pattern;
+
+import org.xbill.DNS.Record;
+import org.xbill.DNS.MXRecord;
+import org.xbill.DNS.Type;
+import org.xbill.DNS.Lookup;
+
+
 
 @Service
 public class EmailValidationService{
@@ -15,15 +23,66 @@ public class EmailValidationService{
 		if (!syntaxValid){
 			return new EmailValidationResponse(
 				email,false,false,"Invalid email format. Exmaple: user@domain.com", null);
-		}
+		}	
 
-		return null;
+		String domain = email.substring(email.indexOf('@')+1);
+
+		MXLookupResult mxResult = performMXLookup(domain);
+
+		return new EmailValidationResponse(
+			email,true, mxResult.hashMX,
+			mxResult.message,
+			mxResult.mxRecords
+		);
 	}
+
 	private boolean validSyntax(String email){
 		if (email==null || email.isEmpty()){
 			return false;
 		}
 		return EMAIL_PATTERN.matcher(email).matches();
+	}
+
+	private MXLookupResult performMXLookup(String domain){
+		try{
+			Record[] records = new Lookup(domain, Type.MX).run();
+			if (records!=null && records.length>0){
+				StringBuilder sb = new StringBuilder();
+
+				for(Record record: records) {
+					MXRecord mx = (MXRecord) record;
+					sb.append(mx.getTarget())
+						.append(" (priority: ")
+						.append(mx.getPriority())
+						.append(")\n");
+				}
+				return new MXLookupResult(true,"MX records found", sb.toString());
+			}
+			// fallback call : check A record
+			Record[] aRecords = new Lookup(domain,Type.A).run();
+
+			if (aRecords!=null && aRecords.length>0){
+				return new MXLookupResult(
+					true, "No MX records exists (may still accept email",
+				"A record fallback"
+			);
+			}
+			return new MXLookupResult(false, "No mail servers found", null);
+		}
+		catch(Exception e){
+			return new MXLookupResult(false,"DNS lookup failed: "+ e.getMessage(), null);
+		}
+	}
+
+	private static class MXLookupResult{
+		boolean hashMX;
+		String message;
+		String mxRecords;
+		MXLookupResult(boolean hashMX, String message, String mxRecords){
+			this.hashMX=hashMX;
+			this.message=message;
+			this.mxRecords=mxRecords;
+		}
 	}
 
 }	
